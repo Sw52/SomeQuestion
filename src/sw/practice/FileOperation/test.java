aa36e8cc7614ed44f155e801320ab40c19517d95
youngsw <798640762@qq.com>
2019-12-29 19:34:55 +0800

    update doc

--- src/sw/practice/FileOperation/test.java
+++ src/sw/practice/FileOperation/test.java
@@ -1,5 +1,8 @@
 package sw.practice.FileOperation;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class test {
@@ -8,6 +11,38 @@ public class test {
         //create file or directory
 //        FileOperation.createFile("/home/swyoung/Desktop/hhh");
 //        FileOperation.createDirectory("/home/swyoung/Desktop/hhhhh");
+        File file = new File("/home/swyoung/Desktop/jiujiu");
+        System.out.println(file.getParent());
+
+        StringBuffer stringBuffer = new StringBuffer();
+        FileReader fileReader = new FileReader(file);
+        char[] s = new char[(int) file.length()];
+        fileReader.read(s);
+        for (char c:s
+             ) {
+            stringBuffer.append(c);
+        }
+        System.out.println(stringBuffer);
+        System.out.println("--------------------");
+
+        FileWriter fileWriter = new FileWriter(file,true);
+        fileWriter.write("ssssfaivnasdvasoivdsovidvi avid我因笑\n");
+        fileWriter.flush();
+        fileWriter.close();
+
+        s = new char[(int) file.length()];
+        fileReader.read(s);
+        fileReader.close();
+        for (char c:s
+        ) {
+            if (c!=0){
+                stringBuffer.append(c);
+            }
+        }
+        System.out.println(stringBuffer);
+        System.out.println("--------------------");
+
+
 
         //delete file or directory
 //        FileOperation.deleteFile("/home/swyoung/Desktop/hhh");
@@ -19,8 +54,8 @@ public class test {
 //        System.out.println(FileOperation.readFileByChars("/home/swyoung/Desktop/jiujiu"));
 //        System.out.println(FileOperation.readFileByLines("/home/swyoung/Desktop/jiujiu"));
 
-        //write to file
-        FileOperation.writeFileByFileOutputStream("/home/swyoung/Desktop/jiujiu", "11111");
-        FileOperation.writeFileByFileWriter("/home/swyoung/Desktop/jiujiu", "2222222");
+//        //write to file
+//        FileOperation.writeFileByFileOutputStream("/home/swyoung/Desktop/jiujiu", "11111");
+//        FileOperation.writeFileByFileWriter("/home/swyoung/Desktop/jiujiu", "2222222");
     }
 }
